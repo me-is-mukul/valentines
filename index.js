2e437c2d7519dba377a750bf994677f6f2fc000e
me-is-mukul <me.is.mukul.2005@gmail.com>
2025-05-23 21:33:15 +0530

    hehe

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector(".no-button");
     const yesButton = document.querySelector(".yes-button");
+
+
+    
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
